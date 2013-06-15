163fc3d58dfc5160a3780cec6d1cdb3e55c42240
sirexalot <sirexalot@hotmail.com>
2013-06-15 13:57:49 +0200

    neue AbstractBeobachter

---
+++ src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/kasse/Beobachter.java
@@ -0,0 +1,9 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.kasse;
+
+public interface Beobachter{
+
+	/**
+	 * Aktualisiert Informationen und ggf. Änderungen
+	 */
+	public void reagiereAufAenderungen();
+}
